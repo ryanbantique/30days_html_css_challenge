64618dbb2c12e1faf90606e5ce4091cedafc5a91
Ryan OFC <ryanjaybantiquee@gmail.com>
2022-11-14 08:25:47 +0800

    Added Cards & Quiz_App

---
+++ Quiz_App/main.js
@@ -0,0 +1,18 @@
+const quizData = [
+  {
+    question: "What is my name?",
+    a: "Java",
+    b: "html",
+    c: "css",
+    d: "javascript",
+    correct: "d",
+  },
+  {
+    question: "What is my name?",
+    a: "Java",
+    b: "html",
+    c: "css",
+    d: "javascript",
+    correct: "d",
+  },
+];
